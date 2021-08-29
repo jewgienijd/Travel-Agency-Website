e7982e9037b99b82c053523967fe3d7e5ea43656
SirJohnSem <jewgienijdanilczik@mail.ru>
2021-08-29 20:11:50 +0200

    add menu

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+const menuIcon = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+
+menuIcon.addEventListener('click', function(){
+    menuBody.classList.toggle('_active');
+    menuIcon.classList.toggle('_active');
+})
